b8a1ae19c9fb0531aa38626cc2646ab7b098cff4
RaisanenMarkus2026 <markus.raisanen@jedu.fi>
2026-08-26 14:39:52 +0300

    Eka PHP-koodi

--- register.php
+++ register.php
@@ -11,10 +11,10 @@
     <h2>Rekisteröityminen</h2>
     <form action="" method="post">
         <label for="name">Uusi opettaja:</label>
-        <input type="text" id="name" name="name">
+        <input type="text" id="name" name="name" required>
         <br>
         <label for="password">Salasana:</label>
-        <input type="password" id="password" name="password">
+        <input type="password" id="password" name="password" required>
         <br>
         <input type="submit" value="Lisää">
     </form>
@@ -23,3 +23,39 @@
     </footer>
 </body>
 </html>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+        
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+
+        // Yhdistä tietokantaan
+        $conn = new mysqli("localhost", "root", "", "taitaja");
+
+        // Tarkista yhteys
+        if ($conn->connect_error) {
+            die("Yhteys epäonnistui: " . $conn->connect_error);
+        }
+
+        // Tarkista, onko opettajan nimi jo olemassa
+        $checkSql = "SELECT * FROM teachers WHERE username='$name'";
+        $result = $conn->query($checkSql);
+        if ($result->num_rows > 0) {
+            echo "Opettajan nimi on jo käytössä. Valitse toinen nimi.";
+            $conn->close();
+            exit();
+        }
+        // Lisää uusi opettaja tietokantaan
+        $sql = "INSERT INTO teachers (username, password_hash) VALUES ('$name', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Uusi opettaja lisätty onnistuneesti.";
+        } else {
+            echo "Virhe: " . $sql . "<br>" . $conn->error;
+        }
+
+        // Sulje yhteys
+        $conn->close();
+    }
+?>
